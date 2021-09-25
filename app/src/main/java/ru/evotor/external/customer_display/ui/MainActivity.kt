5c19eee337b6d3e039b7a68ed3a4f08ffd8775f2
rampbunker <rampbunker@gmail.com>
2021-09-26 00:44:21 +0300

    Add Toolbar for start screen

--- app/src/main/java/ru/evotor/external/customer_display/ui/MainActivity.kt
+++ app/src/main/java/ru/evotor/external/customer_display/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.evotor.external.customer_display.ui
 
 import android.os.Bundle
+import android.view.Menu
 import dagger.android.support.DaggerAppCompatActivity
 import ru.evotor.external.customer_display.R
 
@@ -18,5 +19,10 @@ class MainActivity : DaggerAppCompatActivity() {
             .replace(R.id.main_container, StartFragment())
             .addToBackStack(StartFragment::class.java.canonicalName)
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.start_menu, menu)
+        return true
     }
 }
--- app/src/main/java/ru/evotor/external/customer_display/ui/StartFragment.kt
+++ app/src/main/java/ru/evotor/external/customer_display/ui/StartFragment.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_start.*
 import ru.evotor.external.customer_display.R
 
+
 class StartFragment : Fragment() {
+
+    private val mainActivity by lazy { activity as MainActivity }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? =
         inflater.inflate(R.layout.fragment_start, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainActivity.setSupportActionBar(startToolbar)
+    }
 }
