a2d349998ca53d46973015e493bd97534220c957
rampbunker <rampbunker@gmail.com>
2021-10-06 07:16:29 +0300

    Add comment on Error

--- app/src/main/java/ru/evotor/external/customer_display/ui/display/InfiniteSlideshowAdapter.kt
+++ app/src/main/java/ru/evotor/external/customer_display/ui/display/InfiniteSlideshowAdapter.kt
@@ -34,8 +34,12 @@ class InfiniteSlideshowAdapter(
         viewType: Int
     ) {
         val imageView = convertView.findViewById<ImageView>(R.id.slideshowImage)
+
+        //Если ставить приложение поверх старого, на этой строчке упадет с ошибкой:
+        //java.lang.SecurityException: Permission Denial: opening provider com.android.providers.media.MediaDocumentsProvider from ProcessRecord{6399b0e 19804:ru.evotor.external.customer_display/u0a401} (pid=19804, uid=10401) requires that you obtain access using ACTION_OPEN_DOCUMENT or related APIs
         val inputStream: InputStream? =
             convertView.context.contentResolver.openInputStream(Uri.parse(itemList?.get(listPosition)?.uriString))
+
         val drawable =
             Drawable.createFromStream(inputStream, itemList?.get(listPosition)?.uriString)
         Glide.with(convertView.context)
